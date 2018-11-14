a58deb0c6be19062e209236ee2303a57bceb384f
FABIOGYN <fabioti1607@gmail.com>
2018-11-14 18:29:23 -0200

    Organizando o script

--- 01-repeated-files.sh
+++ 01-repeated-files.sh
@@ -1,5 +1,11 @@
-!#/bin/bash
+#!/bin/bash
 
 #Shebang estatico
 
-##Script para encontrar arquivos repetidos e criar relacao de arquivos de varios discos.
+##Script para encontrar arquivos repetidos pela comparacao de hash md5 e criar relacao de arquivos de varios discos.
+
+clear;
+echo "";
+#find $@ -type f -exec md5sum {} + | uniq -D -w32 | sort;
+
+find $@ -type f -print0 | xargs -0 -I "{}" sh -c 'md5sum "{}" |  cut -f1 -d " " | tr "\n" " "; du -h "{}"' | sort -h -k2 -r | uniq -w32 --all-repeated=separate
