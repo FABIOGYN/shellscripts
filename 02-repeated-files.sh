aee685d343b9c5714d4ddc2120ce38ffed7485f6
FABIOGYN <fabioti1607@gmail.com>
2018-11-17 00:08:29 -0200

    Find com Output para um arquivo e inicio do laço while.

--- 02-repeated-files.sh
+++ 02-repeated-files.sh
@@ -8,4 +8,20 @@ clear;
 echo "";
 #find $@ -type f -exec md5sum {} + | uniq -D -w32 | sort;
 
-find $@ -type f -print0 | xargs -0 -I "{}" sh -c 'md5sum "{}" |  cut -f1 -d " " | tr "\n" " "; du -h "{}"' | sort -h -k2 -r | uniq -w32 --all-repeated=separate
+#find $@ -type f -print0 | xargs -0 -I "{}" sh -c 'md5sum "{}" |  cut -f1 -d " " | tr "\n" " "; du -h "{}"' | sort -h -k2 -r | uniq -w32 --all-repeated=separate
+
+find $@ -type f -print0 | xargs -0 -I "{}" sh -c 'md5sum "{}"' | sort -h -k2 -r | uniq -w32 --all-repeated=separate > /tmp/listagem-repetidos.txt
+echo "";
+
+#exec 3
+
+while read line;
+do
+
+	firstline=$(head -c 1 /tmp/listagem-repetidos.txt | cut -d " " -f1);
+	arr-repetidos=$(grep $firstline /tmp/listagem-repetidos.txt);
+
+	echo "$firstline";
+	echo "";
+	echo "$arr-repetidos";
+done < /tmp/listagem-repetidos.txt;
